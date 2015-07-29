894450432baf2386d41147623e666e98d1db2ec8
Artem Miroshnyk <artyom.miroshnik@propeople.com.ua>
2015-07-29 23:57:03 +0300

    Fix smartling entity definition, setup account info and expert smartling config forms

--- src/Controller/AdminSettingsController.php
+++ src/Controller/AdminSettingsController.php
@@ -5,15 +5,16 @@
  */
 
 namespace Drupal\smartling\Controller;
+use Drupal\Core\Form\FormInterface;
 
 /**
  * AdminSettingsController.
  */
 class AdminSettingsController {
 
-  private function wrapInFieldset(array $form, $title) {
-    return array(
-      '#type' => 'details',//'fieldset',
+  private function wrapWithFieldset(FormInterface $form, $title) {
+    return [
+      '#type' => 'details',
       '#group' => 'smartling',
       '#title' => $title,
       '#attributes' => array(
@@ -21,7 +22,7 @@ class AdminSettingsController {
         'id' => array('smartling-' . strtolower(str_replace(' ', '-', $title))),
       ),
       'children' => $form,
-    );
+    ];
   }
 
   /**
@@ -30,31 +31,32 @@ class AdminSettingsController {
   public function settingsPage() {
     // Add ajax library.
    // drupal_add_library('system', 'drupal.ajax');
-    $output['message'] = array(
+    $output['message'] = [
       '#type' => 'markup',
       '#title' => 'Link to submission views',
       '#title_display' => 'invisible',
     //  @ToDo:
     //  '#prefix' => t('After you configure Smartling module you can <a href="@url">start submitting your content</a>.', array('@url' => url('admin/content/smartling-content'))),
-    );
+    ];
 
-    $output['smartling'] = array(
-//      '#type' => 'vertical_tabs',
-//      '#title' => '123'
+    $output['smartling'] = [
+      '#type' => 'details',
+      '#theme_wrappers' => array(),
 //      '#attached' => array(
 //        'js' => array(drupal_get_path('module', 'smartling') . '/js/smartling_admin.js'),
 //        'css' => array(drupal_get_path('module', 'smartling') . '/css/smartling_admin.css'),
 //      ),
-    );
+    ];
 
     $settings_forms = [
-      'Drupal\smartling\Form\AdminAccountInfoSettingsForm' => 'Account info',
+      'Drupal\smartling\Form\AccountInfoSettingsForm' => 'Account info',
+      'Drupal\smartling\Form\ExpertInfoSettingsForm' => 'Expert info',
     ];
     //module_invoke_all('smartling_settings_form_info');
 
     foreach ($settings_forms as $machine_name => $title) {
-      $form = \Drupal::formBuilder()->getForm($machine_name);//drupal_get_form($machine_name);
-      $output['smartling'][] = $this->wrapInFieldset($form, $title);
+      $form = \Drupal::formBuilder()->getForm($machine_name);
+      $output[$machine_name . '_details'] = $this->wrapWithFieldset($form, $title);
     }
 
     return $output;
--- src/Entity/SmartlingEntityData.php
+++ src/Entity/SmartlingEntityData.php
@@ -6,7 +6,12 @@
  */
 
 namespace Drupal\smartling\Entity;
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Entity\Annotation\ContentEntityType;
+use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\Entity;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
 
 /**
  * Defines the node entity class.
@@ -26,4 +31,107 @@ use Drupal\Core\Entity\Entity;
  *
  * )
  */
-class SmartlingEntityData extends Entity {}
+class SmartlingEntityData extends ContentEntityBase {
+  /**
+   * Provides base field definitions for an entity type.
+   *
+   * Implementations typically use the class
+   * \Drupal\Core\Field\BaseFieldDefinition for creating the field definitions;
+   * for example a 'name' field could be defined as the following:
+   * @code
+   * $fields['name'] = BaseFieldDefinition::create('string')
+   *   ->setLabel(t('Name'));
+   * @endcode
+   *
+   * By definition, base fields are fields that exist for every bundle. To
+   * provide definitions for fields that should only exist on some bundles, use
+   * \Drupal\Core\Entity\FieldableEntityInterface::bundleFieldDefinitions().
+   *
+   * The definitions returned by this function can be overridden for all
+   * bundles by hook_entity_base_field_info_alter() or overridden on a
+   * per-bundle basis via 'base_field_override' configuration entities.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition. Useful when a single class is used for multiple,
+   *   possibly dynamic entity types.
+   *
+   * @return \Drupal\Core\Field\FieldDefinitionInterface[]
+   *   An array of base field definitions for the entity type, keyed by field
+   *   name.
+   *
+   * @see \Drupal\Core\Entity\EntityManagerInterface::getFieldDefinitions()
+   * @see \Drupal\Core\Entity\FieldableEntityInterface::bundleFieldDefinitions()
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields['eid'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Smartling submission ID'))
+      ->setDescription(t('The smartling submission entity ID.'))
+      ->setReadOnly(TRUE)
+      ->setSetting('unsigned', TRUE);
+
+    $fields['uuid'] = BaseFieldDefinition::create('uuid')
+      ->setLabel(t('UUID'))
+      ->setDescription(t('The smarting entity UUID.'))
+      ->setReadOnly(TRUE);
+
+    $fields['rid'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Smartling submission\'s related entity ID'))
+      ->setDescription(t('The entity smartling submission stores translation of.'))
+      ->setReadOnly(TRUE)
+      ->setSetting('unsigned', TRUE);
+
+    $fields['entity_type'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Smartling submission\'s related entity type machine name'))
+      ->setDescription(t('Smartling submission\'s related entity type machine name.'))
+      ->setReadOnly(TRUE)
+      ->setSetting('unsigned', TRUE);
+
+    $fields['bundle'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Smartling submission\'s related entity bundle machine name'))
+      ->setDescription(t('Smartling submission\'s related entity bundle machine name.'))
+      ->setReadOnly(TRUE);
+
+    $fields['original_language'] = BaseFieldDefinition::create('language')
+      ->setLabel(t('Original language code'))
+      ->setDescription(t('Original language code (drupal format).'))
+      ->setReadOnly(TRUE)
+      ->setTranslatable(FALSE);
+
+    $fields['target_language'] = BaseFieldDefinition::create('language')
+      ->setLabel(t('Target language code'))
+      ->setDescription(t('Target language code (drupal format).'))
+      ->setReadOnly(TRUE)
+      ->setReadOnly(TRUE)
+      ->setTranslatable(FALSE);
+
+    $fields['title'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Smartling submission entity title'))
+      ->setDescription(t('Smartling submission entity title. Usually it is equal to related entity title.'))
+      ->setReadOnly(TRUE);
+
+    $fields['file_name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('File with original content'))
+      ->setDescription(t('File with original content.'))
+      ->setReadOnly(TRUE);
+
+    $fields['translated_file_name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('File with translated content'))
+      ->setDescription(t('File with translated content.'))
+      ->setReadOnly(TRUE);
+
+    $fields['progress'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Smartling submission translation progress'))
+      ->setDescription(t('Smartling submission translation progress.'))
+      ->setReadOnly(TRUE)
+      ->setSetting('unsigned', TRUE);
+
+    $fields['submitter'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Smartling submission submitter'))
+      ->setDescription(t('Smartling submission submitter'))
+      ->setReadOnly(TRUE);
+
+    // @todo add other fields.
+
+    return $fields;
+  }
+}
